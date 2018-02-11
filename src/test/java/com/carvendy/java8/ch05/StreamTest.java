99ef0fc2360d4b192208d76fcdd88d43709f8ca3
carvendy <carvendy@163.com>
2018-02-11 16:32:19 +0800

    5章,代码笔记内容提交.

--- src/test/java/com/carvendy/java8/ch05/StreamTest.java
+++ src/test/java/com/carvendy/java8/ch05/StreamTest.java
@@ -2,8 +2,10 @@ package com.carvendy.java8.ch05;
 
 import static java.util.stream.Collectors.toList;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 import org.junit.Test;
 
@@ -42,6 +44,35 @@ public class StreamTest {
         ).collect(toList());
 
         arrays.forEach(ints -> System.out.println(ints[0]+","+ints[1]));
+    }
+
+
+    @Test
+    public void match(){
+        List<String> person = Arrays.asList("袭人", "晴雯", "宝玉");
+
+        if(person.stream().anyMatch(s -> s.indexOf("玉") > -1)){
+            System.out.println("其中一个有玉");
+        }
+
+        person = Arrays.asList("贾宝玉", "林黛玉", "蒋玉涵","妙玉");
+        if(person.stream().anyMatch(s -> s.indexOf("玉") > -1)){
+            System.out.println("全都有玉");
+        }
+        //noneMatch
+        //findFirst()
+    }
+
+    @Test
+    public void sum(){
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5);
+
+        int baseValue = 1;
+        Integer sum = nums.stream().reduce(baseValue, (a, b) -> a + b);
+        System.out.println("sum:"+sum);
+
+        Optional<Integer> reduce = nums.stream().reduce((a, b) -> a + b);
+
     }
 
 
